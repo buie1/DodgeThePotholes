f1fabd5e690f9ef3a42562657567f6b7c5586a91
jab165@duke.edu <jab165@duke.edu>
2016-11-08 01:00:27 -0500

    menu w/ start sound (thanks young!) car won't wrap... overall dopeness

--- DodgeThePotholes/MenuScene.swift
+++ DodgeThePotholes/MenuScene.swift
@@ -20,7 +20,23 @@ class MenuScene: SKScene {
         settingsButtonNode = self.childNode(withName: "SettingsButton") as! SKSpriteNode!
         gameCenterButtonNode = self.childNode(withName: "GameCenterButton") as! SKSpriteNode!
         
-        
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        
+        if let location = touch?.location(in: self) {
+            
+            let nodesArray = self.nodes(at: location)
+            
+            if nodesArray.first?.name == "NewGameButton" {
+                self.run(SKAction.playSoundFileNamed("start.wav", waitForCompletion: false))
+                let transition = SKTransition.flipHorizontal(withDuration: 1.0)
+                let gameScene = GameScene(size: self.size)
+                gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                self.view?.presentScene(gameScene, transition: transition)
+            }
+        
+        }
+    }
 }
