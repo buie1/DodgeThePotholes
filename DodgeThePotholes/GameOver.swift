966417dd9dfccf87ed871abc3220f97277ffefb3
ece <ece@wl-10-190-84-180.wireless.duke.edu>
2016-11-11 00:47:48 -0500

    gameOver work in progress

---
+++ DodgeThePotholes/GameOver.swift
@@ -0,0 +1,54 @@
+//
+//  MenuScene.swift
+//  DodgeThePotholes
+//
+//  Created by Jonathan Buie on 11/7/16.
+//  Copyright © 2016 Jonathan Buie. All rights reserved.
+//
+
+import SpriteKit
+
+class GameOver: SKScene {
+    
+    var score:Int = 0
+    var gameOverLabelNode:SKLabelNode!
+    var scoreLabelNode:SKLabelNode!
+    
+    
+    override func didMove(to view: SKView) {
+        
+        
+        gameOverLabelNode = self.childNode(withName: "gameOverLabel") as! SKLabelNode!
+        gameOverLabelNode.fontName = "PressStart2p"
+        
+        scoreLabelNode = self.childNode(withName: "scoreLabel") as! SKLabelNode!
+        scoreLabelNode.fontName = "PressStart2p"
+        scoreLabelNode.text = "\(score)"
+
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        
+        if let location = touch?.location(in: self) {
+            
+            let nodesArray = self.nodes(at: location)
+            let transition = SKTransition.flipHorizontal(withDuration: 1.0)
+            
+            if nodesArray.first?.name == "NewGameButton" {
+                self.run(SKAction.playSoundFileNamed("start.wav", waitForCompletion: false))
+                let gameScene = GameScene(size: self.size)
+                gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                self.view?.presentScene(gameScene, transition: transition)
+            }
+            else if nodesArray.first?.name == "SettingsButton"{
+                print("Settings Button Was Pushed")
+                let settingsScene = SKScene(fileNamed: "SettingsScene")
+                settingsScene?.scaleMode = .aspectFill
+                self.view?.presentScene(settingsScene!, transition: transition)
+            }
+            
+        }
+    }
+}
