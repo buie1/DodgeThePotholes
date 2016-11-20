aa4396b7a0283fda1cd4cc72020ef7d737544350
cts20@duke.edu <cts20@duke.edu>
2016-11-20 18:26:48 -0500

    Adding Highscores Scene

---
+++ DodgeThePotholes/Highscores.swift
@@ -0,0 +1,26 @@
+//
+//  Highscores.swift
+//  DodgeThePotholes
+//
+//  Created by Colby Stanley on 11/20/16.
+//  Copyright © 2016 Jonathan Buie. All rights reserved.
+//
+
+import SpriteKit
+
+class HighscoresScene: SKScene {
+    
+    
+    
+    override func didMove(to view: SKView) {
+    
+        
+        
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     
+    }
+}
