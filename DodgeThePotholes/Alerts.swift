6e31c4d05c3c2aed596955610f36bbda1e4c0a29
jab165@duke.edu <jab165@duke.edu>
2016-12-04 00:33:54 -0500

    alerts working.

--- DodgeThePotholes/Alerts.swift
+++ DodgeThePotholes/Alerts.swift
@@ -31,3 +31,45 @@ extension Alerts where Self: GameOverScene {
     }
     
 }
+
+extension Alerts where Self: SKScene{
+    func doPurchase(title: String, message: String, cost: Int, item: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            //update money
+            preferences.set(preferences.value(forKey: "money") as! Int - cost, forKey: "money")
+            //update unlocked items
+            preferences.set(true, forKey: item)
+        }
+        let noAction = UIAlertAction(title: "No", style: .cancel){ _ in}
+        
+        alertController.addAction(yesAction)
+        alertController.addAction(noAction)
+        self.view?.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+    
+    func insufficientFunds(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .cancel){ _ in}
+        alertController.addAction(okAction)
+        self.view?.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+    
+    func notUnlocked(){
+        let alertController = UIAlertController(title: "Item Locked", message: "This item is not yet unlocked! You must first purchase it in the store!", preferredStyle: .alert)
+        
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel){ _ in}
+        alertController.addAction(dismissAction)
+        self.view?.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+    
+    func alreadyPurchased(){
+        let alertController = UIAlertController(title: "Item Already Purchased!", message: "You have this item!", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel){ _ in}
+        alertController.addAction(dismissAction)
+        self.view?.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+}
